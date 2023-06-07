14cf7869d114d8be6104e5a868c65f16c2ec797f
Fedor <4ips228@gmail.com>
2023-06-07 22:33:34 +0700

    Home Work sem3

--- src/main/java/homeWorkSem3LinkedList/Main.java
+++ src/main/java/homeWorkSem3LinkedList/Main.java
@@ -14,23 +14,23 @@ public class Main {
 
         System.out.printf("list size: %d\n", linkedList.size());
 
-        System.out.println("------");
+        System.out.println("---Размер---");
         for (String value : linkedList) {
             System.out.println(value);
         }
 
-        System.out.println("---delite-index-(enter index)---");
+        System.out.println("\n---Удалить элемент(enter index)---");
         System.out.println(linkedList.remove(5));
 
-        System.out.println("---set-element-index-1--");
-        linkedList.set("new value", 1);
+        System.out.println("\n---Заменить элемент (enter index)--");
+        linkedList.set("Hello World!", 4);
 
-        System.out.println("---get-elements-by-index---");
+        System.out.println("\n---get-elements-by-index---");
         for (int i = 0; i < linkedList.size(); i++) {
             System.out.println(linkedList.get(i));
         }
 
-        System.out.println("---clear---");
+        System.out.println("\n---Удалить список---");
         linkedList.clear();
         System.out.printf("list size: %d\n", linkedList.size());
     }
